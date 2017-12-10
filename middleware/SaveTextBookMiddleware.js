2c66ea1fce8d1e7fcc79c1653bc17ab8e7a58fee
NaifAlrashed <naialrashed@alfaisal.edu>
2017-12-10 11:01:51 +0300

    refactor
    add the new changes to the app. still not tested yet

--- middleware/SaveTextBookMiddleware.js
+++ middleware/SaveTextBookMiddleware.js
@@ -1,35 +1,41 @@
 const saveTextBookBehavior = require('../controllers/saveTextBook')
+const responseTypes = require('../controllers/responseTypes')
 
-module.exports = {
-    saveUniverityIfNotExist: async (req, res, next) => {
-        let university = await saveTextBookBehavior.doesUniversityExist(req.body.university)
-        if (!university) {
-            university = await saveTextBookBehavior.saveUniversity(req.body.university)
-        }
-        req.universityId = university._id
-        next()
-    },
-
-    saveDepartmentIfNotExist: async (req, res, next) => {
-        let department = await saveTextBookBehavior.doesDepartmentExist(req.body.department, req.universityId)
-        if (!department) {
-            department = await saveTextBookBehavior.saveDepartment(req.body.department, req.universityId)
-        }
-        req.departmentId = department._id
+exports.saveUniverityIfNotExist = async (req, res, next) => {
+    let universityObj = await saveTextBookBehavior.saveUniverityIfNotExist(req.body.university)
+    if (isResourceSavedOrRetrieved(universityObj)) {
+        req.university = universityObj.resource
         return next()
-    },
-
-    saveCourseIfNotExist: async (req, res, next) => {
-        let course = await saveTextBookBehavior.doesCourseExist(req.body.resource, req.departmentId)
-        if (!course) {
-            course = await saveTextBookBehavior.saveCourse(req.body.resource, req.departmentId)
-        }
-        req.courseId = course._id
-        next()
-    },
-
-    saveBook: async (req, res) => {
-        const book = await saveTextBookBehavior.saveBook(req.body.book, req.user._id, req.courseId)
-        res.status(201)
+    } else {
+        return res.status(universityObj.status).json(universityObj.response())
     }
+}
+
+exports.saveDepartmentIfNotExist = async (req, res, next) => {
+    let departmentObj = await saveTextBookBehavior.saveDepartmentIfNotExist(req.body.department, req.university)
+    if (isResourceSavedOrRetrieved(departmentObj)) {
+        req.department = departmentObj.resource
+        return next()
+    } else {
+        return res.status(departmentObj.status).json(departmentObj.response())
+    }
+}
+
+exports.saveCourseIfNotExist = async (req, res, next) => {
+    let courseObj = await saveTextBookBehavior.saveCourseIfNotExist(req.body.resource, req.department)
+    if (isResourceSavedOrRetrieved(courseObj)) {
+        req.course = courseObj.resource
+        return next()
+    } else {
+        return res.status(courseObj.status).json(courseObj.response())
+    }
+}
+
+exports.saveBook = async (req, res) => {
+    const bookObj = await saveTextBookBehavior.saveBook(req.body.book, req.user, req.course)
+    res.status(bookObj.status).json(bookObj.response())
+}
+
+const isResourceSavedOrRetrieved = (resource) => {
+    return (resource.type === responseTypes.RESOURCE_CREATED || resource.type === responseTypes.RESOURCE_FOUND)
 }
